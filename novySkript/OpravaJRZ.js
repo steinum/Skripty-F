35a6ad72e57ecfe344532f489feca9ebec302361
Steinum <eduardbrody@gmail.com>
2019-04-11 11:29:17 +0200

    Uprava poradia a doplnenie kodu

--- novySkript/OpravaJRZ.js
+++ novySkript/OpravaJRZ.js
@@ -3,44 +3,83 @@
 //Skript opravuje import osob z JRZ
 var scriptName = "OpravaJRZ";
 
-try
-{
-
-coort.Trace(scriptName + "  START -->");
-
 //---------------------CONFIGURABLES---------------------------
 var inFile = "D:\\jrz\\CSV_od_Jakuba_COO_adresy_prvy_import_mapovanie_osoby_adresy.csv";
 var logDirPath = "D:\\jrz\\";
 var doLogFile = true;
 
+//----------------------Functions-----------------------------------------------------
+
+function IsNullOrEmpty(inStr)
+{
+	if (inStr==null || inStr=="")
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+//----------------------MAIN_CODE-----------------------------------------------------
+
+
+try
+{
+
+coort.Trace(scriptName + "  START -->");
+
 //create log file
-var dateFormated = GetFormatedDate(startDate);
 var logPath = logDirPath + scriptName + ".txt";
 var fso = new ActiveXObject("Scripting.FileSystemObject");
 var logFile = null;
 if (doLogFile)
 {
+  coort.Trace("  >>> VYTVARAM LOGFILE  ");
   logFile = fso.CreateTextFile(logPath, true);
 }
 
 //nacitanie csv - read file
+coort.Trace("  ::: otvaram csv subor   ", fso.FileExists(inFile));
+if (fso.FileExists(inFile)) {
 var inFile = fso.OpenTextFile(inFile, 1);
+}
+
 var lineNum = 0;
 while (!inFile.AtEndOfStream) // prvy riadok
 {
+  coort.Trace("  AtEdnOfStream:  ", lineNum );
   lineNum++;
   var fileLine = inFile.ReadLine(); // nacitaj riadok
+  coort.Trace(" fileLine:  ", fileLine );
+
   try
   {
+    coort.Trace(" try2  ");
     if ( !IsNullOrEmpty(fileLine)) // ak riadok nie je prazdny
+    coort.Trace(" !IsNullOrEmpty(fileLine)  ", !IsNullOrEmpty(fileLine));
     {
       var fileLineArr = fileLine.split(";"); // pozrie po prvu bodkociarku
+      coort.Trace(" fileLineArr:  ", fileLineArr);
       if (fileLineArr.length>1) // ak daka je
       {
 
         // trace toho co vypise
         coort.Trace("fileLine", fileLine);
         coort.Trace("fileLineArr", fileLineArr);
+        coort.Trace("fileLineArr[0] entitiID", fileLineArr[0]);
+        coort.Trace("fileLineArr[1] osobaCoo", fileLineArr[1]);
+        coort.Trace("fileLineArr[2] DruhAdresy", fileLineArr[2]);
+        coort.Trace("fileLineArr[3] adresaCOO", fileLineArr[3]);
+
+
+
+        if (doLogFile)
+  			{
+  				logFile.WriteLine("fileLine: " + fileLine);
+  			}
 
 /*
         var identifier = fileLineArr[0];
